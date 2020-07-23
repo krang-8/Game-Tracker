5b13c7a268b3ed5445c38e81245ed5fe21cb1012
krang-8 <55773939+krang-8@users.noreply.github.com>
2020-07-24 01:53:49 +0400

    Add files via upload
    Latest version, forgot to post earlier versions to Github so removing them from memory. 
    
    Current features : 
    - Retrieve match details from FACEIT API
    - Runs discord bot
    - Sends messages with matchroom links at the request of a !matches command on discord
    - Uses Selenium integration to grab a screenshot and post it along with the matchroom link (will probably be removed later as it is too slow)

---
+++ GameTrackerV0.1.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import discord
+from discord.ext import commands
+from Screenshot import Screenshot_Clipping
+from selenium import webdriver
+import time
+
+
+headers = {
+        'accept': 'application/json',
+        'Authorization': 'Bearer c5c95723-fbbe-45a3-b512-41a18c1acac4',
+    }
+
+params = (
+        ('type', 'ongoing'),
+        ('offset', '0'),
+        ('limit', '20'),
+    )
+
+def liveGames(headers,params):
+
+
+    hubList = ['bed6a13f-6aa3-4d01-839f-b83a9d26c589', '627e10d3-00ac-4376-9f62-a22812e220b1',
+               'e244b64a-2a54-43a2-8a0e-df2380a8c1cc', 'e7ee1bc8-2be1-4512-955e-5542daae4152',
+               '73b8f93e-6a49-4abd-90e7-71caa4735c04', '89b35fc4-cc30-4966-842e-9d5d3a47d09c']
+
+    matchList = []
+
+    for hub in hubList:
+
+        requestURL = 'https://open.faceit.com/data/v4/hubs/' + hub + '/matches'
+        response = requests.get(requestURL, headers=headers, params=params)
+        matchDeets = response.json()
+        packageName = "items"
+        gameRoom = "match_id"
+        players = ""
+
+        matchPackage = json.dumps(matchDeets, indent = 2)
+        testVar = True
+        COUNTER = 0
+        while testVar != False:
+            try:
+                matchPackage = json.dumps(matchDeets[packageName][COUNTER][gameRoom])
+                matchLink = "https://www.faceit.com/en/csgo/room/"+matchPackage
+                fixedMatchLink = matchLink.replace('"', '')
+                if fixedMatchLink not in matchList :
+                    matchList.append(fixedMatchLink)
+
+
+                COUNTER += 1
+            except:
+                testVar = False
+
+
+    return matchList
+
+
+
+def screenshotMatchpage(match):
+    ob = Screenshot_Clipping.Screenshot()
+    driver = webdriver.Chrome('E:\chromedriver.exe')
+    url = match
+    driver.get(url)
+    time.sleep(1)
+    img_url = ob.full_Screenshot(driver, save_path=r'C:\Users\krang\Desktop\matches', image_name='Myimage.png')
+    driver.close()
+    driver.quit()
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+
+    if message.content.find("!matches") != -1:
+        matchList = liveGames(headers,params)
+        for match in matchList:
+            await message.channel.send(match)
+            screenshotMatchpage(match)
+            await message.channel.send(file=discord.File(r'C:\Users\krang\Desktop\matches\Myimage.png'))
+
+
+    if message.content.find("!hello") != -1:
+        await message.channel.send("Hi") # If the user says !hello we will send back hi
+
+client.run("NzM1OTAxMzE1MDcwMTY1MTQy.Xxn4gQ.bzkJIP25cORpqM0VvmhfwJGkK2o")
