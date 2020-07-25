cd31fb60f20e8de4af5d1a89eab5a0182912b4b4
krang-8 <55773939+krang-8@users.noreply.github.com>
2020-07-26 01:11:40 +0400

    Update V0.3
    Updated Functionalities : 
    
    - Properly implemented Map
    - No longer posts matches that are still in voting process
    - Changed how player details are parsed, quicker and less resources

--- src/GameTrackerV0.3.py
+++ src/GameTrackerV0.3.py
@@ -2,10 +2,8 @@ import requests
 import json
 import discord
 from discord.ext import commands
-from Screenshot import Screenshot_Clipping
-from selenium import webdriver
 import time
-from PIL import Image
+
 
 
 headers = {
@@ -27,9 +25,7 @@ def liveGames(headers,params):
                '73b8f93e-6a49-4abd-90e7-71caa4735c04', '89b35fc4-cc30-4966-842e-9d5d3a47d09c']
 
     matchList = []
-    playerList1 = []
-    playerList2 = []
-    factionList = ['faction1', 'faction2']
+
 
     for hub in hubList:
 
@@ -46,60 +42,63 @@ def liveGames(headers,params):
         while testVar != False:
             try:
                 matchPackage = json.dumps(matchDeets[packageName][COUNTER][gameRoom])
-                for faction in factionList:
-                    for count in range(0, 5):
-                        player = json.dumps(
-                            matchDeets[packageName][COUNTER]['teams'][faction]['roster'][count]['nickname'],indent=2).replace('"', '')
-                        if faction == 'faction1':
-                            if player not in playerList1:
-                                playerList1.append(player)
-                        if faction == 'faction2':
-                            if player not in playerList2:
-                                playerList2.append(player)
                 matchLink = "https://www.faceit.com/en/csgo/room/"+matchPackage
                 fixedMatchLink = matchLink.replace('"', '')
-
                 if fixedMatchLink not in matchList :
                     matchList.append(fixedMatchLink)
-
-
                 COUNTER += 1
             except:
                 testVar = False
 
 
-    return matchList, playerList1, playerList2
+    return matchList
 
 
 
-def screenshotMatchpage(match):
-    ob = Screenshot_Clipping.Screenshot()
-    driver = webdriver.Chrome('E:\chromedriver.exe')
-    url = match
-    driver.get(url)
-    time.sleep(1)
-    submit_button = driver.find_elements_by_xpath('/html/body/div[1]/div/div/div/div[2]/div[2]/div[1]/button[2]')[0]
-    submit_button.click()
-    img_url = ob.full_Screenshot(driver, save_path=r'C:\Users\krang\Desktop\matches', image_name='Myimage.png')
-    driver.close()
-    driver.quit()
-    im = Image.open(r'C:\Users\krang\Desktop\matches\myImage.png')
-    cropped = im.crop((0, 150, 928, 600))
-    cropped.save(r'C:\Users\krang\Desktop\matches\myImage.png')
+
 
 client = discord.Client()
 
 postedMatches = []
 
-def splitPlayers(playerList1,playerList2):
-    teamListA =[]
-    teamListB =[]
-    teamListA = playerList1[0:5]
-    teamListB = playerList2[0:5]
-    del playerList1[:5]
-    del playerList2[:5]
+def getPlayers(match):
+    teamRequestsURL = 'https://open.faceit.com/data/v4/matches/' + match[36:]
+    requestMatch = requests.get(teamRequestsURL, headers = headers)
+    playerDeets = requestMatch.json()
+    factionList = ['faction1','faction2']
+    playerList1 = []
+    playerList2 = []
+    for faction in factionList:
+        for count in range(0,5):
+            player = json.dumps(playerDeets['teams'][faction]['roster'][count]['nickname']).replace('"', '')
+            if faction == 'faction1':
+                if player not in playerList1:
+                    playerList1.append(player)
+            if faction == 'faction2':
+                if player not in playerList2:
+                    playerList2.append(player)
+    return playerList1,playerList2
 
-    return teamListA, teamListB
+
+def getMap(match):
+    teamRequestsURL = 'https://open.faceit.com/data/v4/matches/' + match[36:]
+    requestMatch = requests.get(teamRequestsURL, headers = headers)
+    mapDeets = requestMatch.json()
+    try:
+        map = json.dumps(mapDeets['voting']['map']['pick'])
+        return map
+    except:
+        pass
+
+def makeEmbed(playerList1,playerList2, match, map):
+    embed = discord.Embed(title="Live Game", description=map, color=0xff5500)
+    embed.set_author(name="MATCH", url=match,icon_url="https://pbs.twimg.com/profile_images/1262464144802013184/BHLstW-J_400x400.jpg")
+    embed.add_field(name="Team A" + "             ",value=playerList1[0] + '\n' + playerList1[1] + '\n' + playerList1[2] + '\n' + playerList1[3] + '\n' + playerList1[4], inline=True)
+    embed.add_field(name="               " + "Team B",value=playerList2[0] + '\n' + playerList2[1] + '\n' + playerList2[2] + '\n' + playerList2[3] + '\n' + playerList2[4], inline=True)
+    embed.set_footer(text="MESL Tracker made by Krang")
+    embed.set_image(url='https://media.discordapp.net/attachments/735918664053948478/736380111665889350/Webp.net-resizeimage_13.png')
+    return embed
+
 
 
 
@@ -108,25 +107,22 @@ def splitPlayers(playerList1,playerList2):
 async def on_ready():
     channelID = client.get_channel(735918664053948478)
     while True:
-        matchList,playerList1,playerList2 = liveGames(headers,params)
+        matchList= liveGames(headers,params)
         for match in matchList:
-            if match not in postedMatches:
-                teamListA, teamListB = splitPlayers(playerList1,playerList2)
-                embed = discord.Embed(title="Live Game",description="Map Name", color = 0xff5500)
-                embed.set_author(name="MATCH",url=match, icon_url = "https://pbs.twimg.com/profile_images/1262464144802013184/BHLstW-J_400x400.jpg")
-                embed.add_field(name="Team A" + "             ", value= teamListA[0] + '\n' + teamListA[1] + '\n' + teamListA[2] + '\n' + teamListA[3] + '\n' + teamListA[4] , inline=True)
-                embed.add_field(name="               " + "Team B", value=teamListB[0] + '\n' + teamListB[1] + '\n' + teamListB[2] + '\n' + teamListB[3] + '\n' + teamListB[4], inline=True)
-                embed.set_footer(text="MESL Tracker made by Krang")
-                embed.set_image(url='https://media.discordapp.net/attachments/735918664053948478/736380111665889350/Webp.net-resizeimage_13.png')
-                await channelID.send(embed=embed)
-                postedMatches.append(match)
-            ### Following else statement will be implemented later, it is about deleting messages of matches that are no longer ongoing.
-            #else:
-                # check match status using API
-                # if status is ongoing, pass
-                # else, delete message from discord
-        time.sleep(60)
+            map = getMap(match)
+            if map != None:
+                map = map.replace('"','').replace('[','').replace(']','')
+                if match not in postedMatches:
+                    playerList1,playerList2 = getPlayers(match)
+                    embed = makeEmbed(playerList1,playerList2,match,map)
+                    await channelID.send(embed=embed)
+                    postedMatches.append(match)
+                ### Following else statement will be implemented later, it is about deleting messages of matches that are no longer ongoing.
+                #else:
+                    # check match status using API
+                    # if status is ongoing, pass
+                    # else, delete message from discord
 
-client.run("discordAPIKEY)
+client.run("discordAPIKEY")
 
 
