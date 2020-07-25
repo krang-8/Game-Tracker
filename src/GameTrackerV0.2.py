9754b59c8c2fc8957a17319b15584943b24ba8f2
krang-8 <55773939+krang-8@users.noreply.github.com>
2020-07-25 06:12:59 +0400

    Add files via upload
    Changed how the bot sends the match info from a link and a screenshot to an embedded message with all the data. Improvements to come.

--- src/GameTrackerV0.2.py
+++ src/GameTrackerV0.2.py
@@ -10,7 +10,7 @@ from PIL import Image
 
 headers = {
         'accept': 'application/json',
-        'Authorization': 'Bearer FACEITapiKEY',
+        'Authorization': 'Bearer c5c95723-fbbe-45a3-b512-41a18c1acac4',
     }
 
 params = (
@@ -27,6 +27,9 @@ def liveGames(headers,params):
                '73b8f93e-6a49-4abd-90e7-71caa4735c04', '89b35fc4-cc30-4966-842e-9d5d3a47d09c']
 
     matchList = []
+    playerList1 = []
+    playerList2 = []
+    factionList = ['faction1', 'faction2']
 
     for hub in hubList:
 
@@ -43,8 +46,19 @@ def liveGames(headers,params):
         while testVar != False:
             try:
                 matchPackage = json.dumps(matchDeets[packageName][COUNTER][gameRoom])
+                for faction in factionList:
+                    for count in range(0, 5):
+                        player = json.dumps(
+                            matchDeets[packageName][COUNTER]['teams'][faction]['roster'][count]['nickname'],indent=2).replace('"', '')
+                        if faction == 'faction1':
+                            if player not in playerList1:
+                                playerList1.append(player)
+                        if faction == 'faction2':
+                            if player not in playerList2:
+                                playerList2.append(player)
                 matchLink = "https://www.faceit.com/en/csgo/room/"+matchPackage
                 fixedMatchLink = matchLink.replace('"', '')
+
                 if fixedMatchLink not in matchList :
                     matchList.append(fixedMatchLink)
 
@@ -54,7 +68,7 @@ def liveGames(headers,params):
                 testVar = False
 
 
-    return matchList
+    return matchList, playerList1, playerList2
 
 
 
@@ -77,17 +91,42 @@ client = discord.Client()
 
 postedMatches = []
 
+def splitPlayers(playerList1,playerList2):
+    teamListA =[]
+    teamListB =[]
+    teamListA = playerList1[0:5]
+    teamListB = playerList2[0:5]
+    del playerList1[:5]
+    del playerList2[:5]
+
+    return teamListA, teamListB
+
+
+
+
 @client.event
 async def on_ready():
     channelID = client.get_channel(735918664053948478)
     while True:
-        matchList = liveGames(headers,params)
+        matchList,playerList1,playerList2 = liveGames(headers,params)
         for match in matchList:
             if match not in postedMatches:
-                await channelID.send(match)
-                screenshotMatchpage(match)
-                await channelID.send(file=discord.File(r'C:\Users\krang\Desktop\matches\Myimage.png'))
+                teamListA, teamListB = splitPlayers(playerList1,playerList2)
+                embed = discord.Embed(title="Live Game",description="Map Name", color = 0xff5500)
+                embed.set_author(name="MATCH",url=match, icon_url = "https://pbs.twimg.com/profile_images/1262464144802013184/BHLstW-J_400x400.jpg")
+                embed.add_field(name="Team A" + "             ", value= teamListA[0] + '\n' + teamListA[1] + '\n' + teamListA[2] + '\n' + teamListA[3] + '\n' + teamListA[4] , inline=True)
+                embed.add_field(name="               " + "Team B", value=teamListB[0] + '\n' + teamListB[1] + '\n' + teamListB[2] + '\n' + teamListB[3] + '\n' + teamListB[4], inline=True)
+                embed.set_footer(text="MESL Tracker made by Krang")
+                embed.set_image(url='https://media.discordapp.net/attachments/735918664053948478/736380111665889350/Webp.net-resizeimage_13.png')
+                await channelID.send(embed=embed)
                 postedMatches.append(match)
+            ### Following else statement will be implemented later, it is about deleting messages of matches that are no longer ongoing.
+            #else:
+                # check match status using API
+                # if status is ongoing, pass
+                # else, delete message from discord
         time.sleep(60)
 
-client.run("discordAPIKEY")
+client.run("NzM1OTAxMzE1MDcwMTY1MTQy.Xxm_gQ.LqkEKucCEq-F3fPCki5W_Q7_L44")
+
+
