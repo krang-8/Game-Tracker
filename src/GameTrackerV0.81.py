0f8b78545c85d540c365d35ec2831428ca1131d4
krang-8 <55773939+krang-8@users.noreply.github.com>
2020-07-27 16:39:08 +0400

    Add files via upload
    Split again, changed how the logic of the whole bot works, back to command instead of on_ready, but command loops itself.

--- src/GameTrackerV0.81.py
+++ src/GameTrackerV0.81.py
@@ -17,39 +17,48 @@ params = (
         ('limit', '20'),
     )
 
+def requestMatch(headers,params,hub):
+    try:
+        requestURL = 'https://open.faceit.com/data/v4/hubs/' + hub + '/matches'
+        response = requests.get(requestURL, headers=headers, params=params)
+        matchDeets = response.json()
+        return matchDeets
+    except :
+        return None
+
 def liveGames(headers,params,hub):
 
 
     matchList = []
 
-    requestURL = 'https://open.faceit.com/data/v4/hubs/' + hub + '/matches'
-    response = requests.get(requestURL, headers=headers, params=params)
-    matchDeets = response.json()
+
     packageName = "items"
     gameRoom = "match_id"
     players = ""
-
+    matchDeets = requestMatch(headers,params,hub)
     testVar = True
     COUNTER = 0
-    while testVar != False:
-        try:
-            matchPackage = json.dumps(matchDeets[packageName][COUNTER][gameRoom])
-            matchLink = "https://www.faceit.com/en/csgo/room/"+matchPackage
-            fixedMatchLink = matchLink.replace('"', '')
-            if fixedMatchLink not in matchList :
-                matchList.append(fixedMatchLink)
-            COUNTER += 1
-        except:
-            testVar = False
+
+    if matchDeets != None:
+        while testVar != False:
+            try:
+                matchPackage = json.dumps(matchDeets[packageName][COUNTER][gameRoom])
+                matchLink = "https://www.faceit.com/en/csgo/room/"+matchPackage
+                fixedMatchLink = matchLink.replace('"', '')
+                if fixedMatchLink not in matchList :
+                    matchList.append(fixedMatchLink)
+                COUNTER += 1
+            except:
+                testVar = False
 
 
-    return matchList
+        return matchList
 
 
 
 
 
-client = discord.Client()
+client = commands.Bot(command_prefix = ".")
 
 postedMatches = []
 
@@ -72,16 +81,6 @@ def getPlayers(match):
     return playerList1,playerList2
 
 
-def getMap(match):
-    teamRequestsURL = 'https://open.faceit.com/data/v4/matches/' + match[36:]
-    requestMatch = requests.get(teamRequestsURL, headers = headers)
-    mapDeets = requestMatch.json()
-    try:
-        map = json.dumps(mapDeets['voting']['map']['pick'])
-        return map
-    except:
-        pass
-
 def makeEmbed(playerList1,playerList2, match, map):
     embed = discord.Embed(title="Live Game", description=map, color=0xff5500)
     embed.set_author(name="MATCH", url=match,icon_url="https://pbs.twimg.com/profile_images/1262464144802013184/BHLstW-J_400x400.jpg")
@@ -92,25 +91,45 @@ def makeEmbed(playerList1,playerList2, match, map):
     return embed
 
 
+def getMap(statusDeets):
+    try:
+        map = json.dumps(statusDeets['voting']['map']['pick'])
+        if map != None:
+            return map
+        else:
+            return None
+    except:
+        return None
 def checkMatch(match):
-    statusRequestsURL = 'https://open.faceit.com/data/v4/matches/' + match[36:]
-    requestStatus = requests.get(statusRequestsURL, headers = headers)
-    statusDeets = requestStatus.json()
-    hubID = json.dumps(statusDeets['competition_id'], indent=2).replace('"','')
-    status = json.dumps(statusDeets['status'], indent=2)
-    return status, hubID
+    try:
+        statusRequestsURL = 'https://open.faceit.com/data/v4/matches/' + match[36:]
+        requestStatus = requests.get(statusRequestsURL, headers = headers)
+        statusDeets = requestStatus.json()
+        hubID = json.dumps(statusDeets['competition_id'], indent=2).replace('"','')
+        status = json.dumps(statusDeets['status'], indent=2)
+        map = getMap(statusDeets)
+
+        return status, hubID, map
+    except:
+        return None, None, None
 
 
 def determineChannel(hub):
     if hub == "627e10d3-00ac-4376-9f62-a22812e220b1":
-        channelID = client.get_channel(735918664053948478)
-        channelID2 = 735918664053948478
-    elif hub == "e244b64a-2a54-43a2-8a0e-df2380a8c1cc":
-        channelID = client.get_channel(736603132796076212)
-        channelID2 = 736603132796076212
+        channelID = client.get_channel(737047691741102160)
+        channelID2 = 737047691741102160
+    elif hub == "e244b64a-2a54-43a2-8a0e-df2380a8c1cc" or hub == "210f559a-1140-46d6-8442-def647daad5d":
+        channelID = client.get_channel(737047648993017967)
+        channelID2 = 737047648993017967
+    elif hub == "bed6a13f-6aa3-4d01-839f-b83a9d26c589":
+        channelID = client.get_channel(737047767129522238)
+        channelID2 = 737047767129522238
+    elif hub == "73b8f93e-6a49-4abd-90e7-71caa4735c04":
+        channelID = client.get_channel(737047728734994462)
+        channelID2 = 737047728734994462
     else:
-        channelID = client.get_channel(736741005713342485)
-        channelID2 = 736741005713342485
+        channelID = client.get_channel(737047813950799942)
+        channelID2 = 737047813950799942
 
     return channelID, channelID2
 
@@ -121,38 +140,95 @@ def getMessageID(matchDictionary, match):
     ID = matchDictionary[match]
     return ID
 
+
+
 @client.event
 async def on_ready():
-    hubList = ['bed6a13f-6aa3-4d01-839f-b83a9d26c589', '627e10d3-00ac-4376-9f62-a22812e220b1',
-               'e244b64a-2a54-43a2-8a0e-df2380a8c1cc', 'e7ee1bc8-2be1-4512-955e-5542daae4152',
-               '73b8f93e-6a49-4abd-90e7-71caa4735c04', '89b35fc4-cc30-4966-842e-9d5d3a47d09c']
+    print('READY')
 
-    while True:
-        for hub in hubList:
-            matchList= liveGames(headers,params,hub)
-            channelID, channelID2 = determineChannel(hub)
-            for match in matchList:
-                map = getMap(match)
-                if map != None:
-                    if match not in postedMatches:
-                        playerList1,playerList2 = getPlayers(match)
-                        map = map.replace('"', '').replace('[', '').replace(']', '')
-                        embed = makeEmbed(playerList1,playerList2,match,map)
-                        embedMSG = await channelID.send(embed=embed)
-                        embedID = embedMSG.id
-                        postedMatches.append(match)
-                        matchDictionary[match] = embedID
+
+
+def getPlayerID(username):
+    requestPlayerURL = 'https://open.faceit.com/data/v4/players?nickname='+username+'&game=COUNTER%20STRIKE%3A%20GLOBAL%20OFFENSIVE'
+    requestPlayerStats = requests.get(requestPlayerURL, headers=headers)
+    statDeets = requestPlayerStats.json()
+    elo = json.dumps(statDeets['games']['csgo']['faceit_elo'], indent=2).replace('"','')
+    level = json.dumps(statDeets['games']['csgo']['skill_level'], indent=2).replace('"', '')
+    playerID = json.dumps(statDeets['player_id'], indent=2).replace('"', '')
+    return playerID,elo,level
+
+
+def getPlayerStats(username):
+    try :
+        playerID,elo,level = getPlayerID(username)
+        requestPlayerURL = 'https://open.faceit.com/data/v4/players/'+playerID+'/stats/csgo'
+        requestPlayerStats = requests.get(requestPlayerURL, headers=headers)
+        statDeets = requestPlayerStats.json()
+        kd = json.dumps(statDeets['lifetime']['Average K/D Ratio'],indent=2)
+        winrate = json.dumps(statDeets['lifetime']['Win Rate %'],indent=2)
+        return kd,elo,level,winrate
+    except :
+        return Null
+
+
+def makePlayerEmbed(colour,kd,elo,img,winrate,username):
+    username.replace('"','')
+    kd.replace('"','')
+    playerEmbed = discord.Embed(title="  ", description="Elo : "+elo, color=colour)
+    playerEmbed.set_thumbnail(url="https://cdn-images-1.medium.com/max/816/1*PwN-Y2RzTdVo2e2wlPv0SQ@2x.png")
+    playerEmbed.set_author(name=username+"'s stats",url="https://www.faceit.com/en/players/"+username, icon_url = img)
+
+    playerEmbed.add_field(name="Average K/D", value=kd, inline=True)
+    playerEmbed.add_field(name="Win Rate %", value=winrate.replace('"','')+"%", inline=True)
+    playerEmbed.set_footer(text="Faceit Stats Tracker by Krang")
+    return playerEmbed
+
+
+
+
+@client.command()
+async def yalla(ctx):
+    if ctx.channel.id == 737047648993017967:
+        hubList = ['bed6a13f-6aa3-4d01-839f-b83a9d26c589', '627e10d3-00ac-4376-9f62-a22812e220b1',
+                   'e244b64a-2a54-43a2-8a0e-df2380a8c1cc', 'e7ee1bc8-2be1-4512-955e-5542daae4152',
+                   '73b8f93e-6a49-4abd-90e7-71caa4735c04', '89b35fc4-cc30-4966-842e-9d5d3a47d09c',
+                   '210f559a-1140-46d6-8442-def647daad5d']
+        loopCount = 0
+        while True:
+            try:
+                for hub in hubList:
+                    matchList = liveGames(headers, params, hub)
+                    channelID, channelID2 = determineChannel(hub)
+                    if 0 <= loopCount <= 4:
+                        await channelID.purge(limit=30)
+                        print('NUKED ' + str(channelID))
+
+                    else:
+                        for match in matchList:
+                            status, hubID, map = checkMatch(match)
+                            if status != '"CANCELLED"' and status != '"FINISHED"' and map != None and match not in postedMatches:
+                                playerList1, playerList2 = getPlayers(match)
+                                map = map.replace('"', '').replace('[', '').replace(']', '')
+                                embed = makeEmbed(playerList1, playerList2, match, map)
+                                embedMSG = await channelID.send(embed=embed)
+                                embedID = embedMSG.id
+                                postedMatches.append(match)
+                                matchDictionary[match] = embedID
+                                print('POSTED ' + str(embedID) + ' + ' + match + ' + ' + status + ' to ' + str(channelID))
+
                     for game in postedMatches:
-                        status,hubID = checkMatch(game)
+                        status, hubID, map = checkMatch(game)
                         if status == '"CANCELLED"' or status == '"FINISHED"':
-                            ID = getMessageID(matchDictionary,game)
+                            ID = getMessageID(matchDictionary, game)
                             IDChannel, IDChannel2 = determineChannel(hubID)
-                            print(ID)
-                            print(game)
                             await client.http.delete_message(IDChannel2, ID)
                             postedMatches.remove(game)
                             matchDictionary.pop(game)
+                            print('DELETED ' + ':' + str(ID) + ' + ' + game + ' + ' + status + ' from ' + str(IDChannel))
 
+                    loopCount += 1
+            except:
+                continue
 
 
 client.run("DISCORDAPIKEY")
